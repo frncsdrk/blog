9f8e44963e9be28c63bec883a62815a59fdf2344
frncsdrk <carlsmoh@gmail.com>
2018-08-25 16:14:32 +0200

    Add sample config

---
+++ peco.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'blog',
+  description: 'my blog - because "EVERYONE" needs one',
+  url: 'https://blog.frncsdrk.co',
+
+  // Generate RSS
+  feed: true,
+  // Used in RSS
+  author: 'frncsdrk'
+}
